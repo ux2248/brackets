ce45f654fa049fd16207a25893573abc6d588de2
Lazzat Mukhametalinova <ux2248@gmail.com>
2021-01-03 01:16:56 +0600

    feat: solution brackets

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    let arr = [];
+    for (let i of bracketsConfig) {
+        i = i.toString().replace(",", "");
+        arr.push(i);
+    }
+    let test = (string) => {
+        for (let j of arr) {
+            if (string.includes(j)) {
+                string = string.replace(j, "");
+            }
+        }
+        str = string;
+        for (let i of arr) {
+            while (str.includes(i)) {
+                test(str);
+            }
+        }
+    }
+    test(str);
+    return str.length === 0;
 }
